21b974bc342352cd741617e25bf02de0c342dd3d
Hugo Drakeskär <hugodrakeskar@gmail.com>
2020-09-10 14:53:07 +0200

    sqr answer finder

--- linear_alg/vectors.py
+++ linear_alg/vectors.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 def matrix_sum(matrix):
@@ -41,10 +42,33 @@ def cross_product(p1, p2, scalar=None):
     if k.is_integer():
         k = int(k)
 
-    return ans, f"{i}i{j if j<0 else '+'+str(j)}j{k if k<0 else '+'+str(k)}k"
+    return {"sum": ans, "text": f"{i}i{j if j<0 else '+'+str(j)}j{k if k<0 else '+'+str(k)}k", "expr": {"i": i, "j": j, "k": k}}
 
 
-print(cross_product((2,-1,2), (-1,-2,1)))
+def expr_len(expr):
+    return math.sqrt(expr["i"]**2 + expr["j"]**2 + expr["k"]**2)
+
+
+# find_square root answer
+def sqrt_ans(num):
+    square = num**2
+    if abs(square - int(square)) < 1/1e8:
+        square = int(square)
+        for i in range(1, 100):
+            seek_factor = math.sqrt(square/i)
+            if seek_factor.is_integer():
+                factor = int(seek_factor)
+                return f"{factor}√{i}"
+
+    return f"√{square}"
+
+
+# p1 = 2, -1, 2
+# p2 = -1, -2, 1
+# prod = cross_product(p1, p2)
+# print(expr_len(prod["expr"])*(1/2))
+length = expr_len({"i": 7, "j": 8, "k": -5})
+print(round(length, 4), sqrt_ans(length))
 
 
 
