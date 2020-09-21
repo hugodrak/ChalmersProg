72854120c904ebfd0736c876668a9f0c2b1c2404
gustav ledous <gustav@ledous.se>
2020-09-21 19:02:02 +0200

    Lab 1: Added download files frmo internet

--- dat425/lab1/topmost.py
+++ dat425/lab1/topmost.py
@@ -1,14 +1,27 @@
 import sys
 from wordfreq import tokenize, countWords, printTopMost
+import urllib.request
 
 
 def main():
     # Loads the files needed for the execution
     stopwords = open(sys.argv[1], "r", encoding="utf8").read().splitlines()
-    text = open(sys.argv[2], "r", encoding="utf8").read().splitlines()
+
+    text_source = sys.argv[2]
+
+    if '://' in text_source:
+        # text-source is an internet url. Download over internet
+        text = urllib.request.urlopen(text_source).read().decode('utf-8')
+    else:
+        # text-source is a local file
+        with open(text_source, mode='r', encoding='utf-8') as f:
+            text = f.read()
+
+    # Split into a list of lines
+    lines = text.splitlines()
 
     limit = int(sys.argv[3])  # sets the limit for how many words will be shown
-    tokens = tokenize(text)  # calls the tokenize function
+    tokens = tokenize(lines)  # calls the tokenize function
     count_list = countWords(tokens, stopwords)  # calls the count word function
     printTopMost(count_list, limit)  # prints the top most words in correct order
 
