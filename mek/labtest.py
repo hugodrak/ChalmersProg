7a4245e452025451bb6e0b667becd3c88e109617
gustav <gustav.ledous@gmail.com>
2021-03-08 11:14:15 +0100

    Exponential-backoff test

--- mek/labtest.py
+++ mek/labtest.py
@@ -1,19 +1,22 @@
 import numpy as np
 import scipy.integrate
 import matplotlib.pyplot as plt
+import math
 
 def f(t,xi):
-    xi_prim = 2*xi
+
+    xi_prim2 = -0.5*xi
+    xi_prim = np.array([-np.exp(-t)])
     return xi_prim
 
 # initial condition
-y0=[0.5]
+y0=[1.0]
 
 # stop time
-tend=10
+tend=5
 
 sol = scipy.integrate.solve_ivp(f,
-                                [0,tend],
+                                [0, tend],
                                 y0,
                                 method='Radau',
                                 t_eval=np.linspace(0,tend,10000))
